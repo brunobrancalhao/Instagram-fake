522d149085d705b990c57bc1b62a9f7b068177ac
Bruno Brancalhao <brunobrancalhao26@gmail.com>
2020-05-30 12:51:41 -0300

    Finish cam

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,9 +1,17 @@
+import {
+  Avatar,
+  Button,
+  Card,
+  Divider,
+  Layout,
+  Text,
+} from '@ui-kitten/components';
+import {Image, ScrollView, StyleSheet, View} from 'react-native';
 import React, {Component} from 'react';
 import {inject, observer} from 'mobx-react';
 
+import {Camera} from '../../components/camera.component';
 import HomeStore from '../../stores/home.store';
-import {Layout, Card, Avatar, Text, Divider} from '@ui-kitten/components';
-import {ScrollView, Image, View, StyleSheet} from 'react-native';
 
 interface Props {
   homeStore: HomeStore;
@@ -19,27 +27,38 @@ export default class Home extends Component<Props> {
   }
 
   render() {
-    const {posts} = this.props.homeStore;
+    const {posts, photoReady, toogleStatus, addPost} = this.props.homeStore;
+
+    const uploadPhoto = (uri: string) => {
+      addPost(uri);
+      toogleStatus(false);
+    };
+
     return (
       <Layout style={{flex: 1}}>
         <ScrollView>
-          {posts.map((post, index) => (
-            <Card key={index} style={styles.card}>
-              <View style={styles.header}>
-                <Avatar
-                  size={'small'}
-                  source={{uri: post.author.avatar}}
-                  style={styles.avatar}
-                />
-                <Text style={styles.title}>{post.author.name}</Text>
-              </View>
-              <Image style={styles.picture} source={{uri: post.image}} />
-              <Divider />
-              <View style={styles.footer}>
-                <Text style={styles.title}>{post.description}</Text>
-              </View>
-            </Card>
-          ))}
+          <Camera status={photoReady} onTakeCamera={uri => uploadPhoto(uri)} />
+          {photoReady === false && (
+            <Button onPress={() => toogleStatus(true)}>Postar</Button>
+          )}
+          {photoReady === false &&
+            posts.map((post, index) => (
+              <Card key={index} style={styles.card}>
+                <View style={styles.header}>
+                  <Avatar
+                    size={'small'}
+                    source={{uri: post.author.avatar}}
+                    style={styles.avatar}
+                  />
+                  <Text style={styles.title}>{post.author.name}</Text>
+                </View>
+                <Image style={styles.picture} source={{uri: post.image}} />
+                <Divider />
+                <View style={styles.footer}>
+                  <Text style={styles.title}>{post.description}</Text>
+                </View>
+              </Card>
+            ))}
         </ScrollView>
       </Layout>
     );
@@ -47,7 +66,11 @@ export default class Home extends Component<Props> {
 }
 
 const styles = StyleSheet.create({
-  card: {padding: 1, margin: 4, backgroundColor: 'black'},
+  card: {
+    padding: 1,
+    margin: 4,
+    backgroundColor: 'black',
+  },
   header: {
     padding: 3,
     alignItems: 'center',
@@ -58,8 +81,14 @@ const styles = StyleSheet.create({
     color: 'white',
     marginHorizontal: 20,
   },
-  avatar: {marginRight: 5},
-  picture: {width: 'auto', minHeight: 200, maxHeight: 500},
+  avatar: {
+    marginRight: 5,
+  },
+  picture: {
+    width: 'auto',
+    minHeight: 200,
+    maxHeight: 500,
+  },
   footer: {
     margin: 4,
     padding: 4,
